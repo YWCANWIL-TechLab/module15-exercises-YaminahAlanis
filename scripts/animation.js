3bf8c4fba7ec2f539578665c9fcd208e31b66f47
gseich <gseich@iastate.edu>
2023-08-29 20:47:51 -0500

    Updated PowerPoint Examples

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,9 +2,9 @@
 function myAnimation() {
     const element = document.getElementById("animate");
     let pos = 0;
-    const id = setInterval(frame, 1000);
+    const id = setInterval(frame, 10);
     function frame() {
-        if(pos === 350) {
+        if(pos === 500) {
             clearInterval(id);
         }
         else {
@@ -13,4 +13,22 @@ function myAnimation() {
             element.style.left = `${pos}px`
         }
     }
+}
+
+function animation2() {
+    const element = document.getElementById("animate");
+    let x = 0;
+    let y = 0;
+    const id = setInterval(frame, 100);
+    function frame() {
+        // Random x position (1300px wide)
+        x = Math.floor(Math.random() * 1170);    
+        y = Math.floor(Math.random() * 450);    // Random y position
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+    }
+
+    const timeoutId = setTimeout(() => {
+        clearInterval(id);
+    }, 5000);
 }
